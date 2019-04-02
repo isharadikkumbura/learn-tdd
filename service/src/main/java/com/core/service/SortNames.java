f3770935b2e7a08de00e78557daf97d48e86d460
Ishara Dikkumbura <ishara.asdk@gmail.com>
2019-04-02 16:12:16 +0530

    Covered Possible Scenarios

--- service/src/main/java/com/core/service/SortNames.java
+++ service/src/main/java/com/core/service/SortNames.java
@@ -1,6 +1,7 @@
 package com.core.service;
 
 import com.sorting.core.Sort;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Iterator;
 import java.util.List;
@@ -14,7 +15,22 @@ public class SortNames {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter comma seperated string Of items to be sorted");
         String itemString = scan.nextLine();
+
+        while (StringUtils.isNumeric(itemString) || itemString.isEmpty()){
+            if (StringUtils.isNumeric(itemString)) {
+                System.out.println("No Characters in the string, Please Re-Enter");
+                itemString = scan.nextLine();
+            }
+            if (itemString.isEmpty()) {
+                System.out.println("No STring found,Please Re-Enter ");
+                itemString = scan.nextLine();
+            }
+
+        }
+
         String newItemString = itemString.replace("\"", "");
+        newItemString = newItemString.replaceAll("\\d", "");
+        newItemString = newItemString.replaceAll("[^a-zA-Z0-9,]", "");
         List<String> nameList = Stream.of(newItemString.split(",")).collect(Collectors.toList());
         Sort sort = new Sort();
         sort.sortNames(nameList);
@@ -22,7 +38,8 @@ public class SortNames {
         while (it.hasNext()) {
             System.out.println(it.next());
         }
-    }
 
+
+    }
 
 }
