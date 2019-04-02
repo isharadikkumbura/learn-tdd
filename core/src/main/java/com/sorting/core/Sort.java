f59d3a1a38984138c08835483d1e476e5b33b3ff
Ishara Dikkumbura <ishara.asdk@gmail.com>
2019-04-02 15:13:43 +0530

    Format Code

--- core/src/main/java/com/sorting/core/Sort.java
+++ core/src/main/java/com/sorting/core/Sort.java
@@ -1,17 +1,12 @@
 package com.sorting.core;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Sort {
-
     public List<String> sortNames(List<String> nameList) {
         Collections.sort(nameList);
-
         return nameList;
     }
-
-
 }
 
